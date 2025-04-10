c83707f6c599606b1e340ef0aae567f3ad85f4ba
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-10 18:31:10 +0200

    1

---
+++ scripts/hero-animate.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heroText = document.querySelector(".hero-text");
+  if (heroText) {
+    heroText.classList.add("animate");
+  }
+});
