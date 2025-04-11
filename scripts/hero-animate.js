ce6ec6b3101349bed228d817c9aa88a3569cf065
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-11 15:44:40 +0200

    1

--- scripts/hero-animate.js
+++ scripts/hero-animate.js
@@ -1,6 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   const heroText = document.querySelector(".hero-text");
   if (heroText) {
     heroText.classList.add("animate");
+  }
+
+  const surveyImage = document.querySelector(".survey-image");
+  const surveyText = document.querySelector(".survey-text");
+
+  if (surveyImage) {
+    surveyImage.classList.add("slide-in-left");
+  }
+  if (surveyText) {
+    surveyText.classList.add("slide-in-right");
   }
 });
