1814ecfdfd85189a6eae89443922ef2050ce954a
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-16 15:14:56 +0200

    1

--- admin/smtp_config.php
+++ admin/smtp_config.php
@@ -4,7 +4,7 @@ define('SMTP_HOST', 'mail.bnbk.hu');
 define('SMTP_PORT', 465);              // SSL port
 define('SMTP_SECURE', 'ssl');          // Biztonságos kapcsolat típusa
 define('SMTP_USER', 'marketing@bnbk.hu');
-define('SMTP_PASS', '513A1D4fc7!'); // A levelezőfiók jelszava
+define('SMTP_PASS', 'Agras01!'); // A levelezőfiók jelszava
 
 define('MAIL_FROM', 'marketing@bnbk.hu');
 define('MAIL_FROM_NAME', 'BNBK Agro');
