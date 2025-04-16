6371f87e3a93680ec4ee3abc2992b00f057a8ab2
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-16 14:13:40 +0200

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->execute([$username]);
     $user = $stmt->fetch();
 
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && md5($password) === $user['password']) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
         $_SESSION['role'] = $user['role'];
