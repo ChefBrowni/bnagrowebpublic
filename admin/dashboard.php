1b226083be381d20ec3df09826ab30408e803cb7
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-18 16:14:32 +0200

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,6 +1,9 @@
 <?php
 require 'session_check.php';
 require 'db.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $page = $_GET['page'] ?? '';
 ?>
