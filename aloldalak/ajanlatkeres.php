51c68fdfc48b866e38abf944070b6feb116c5f3b
ChefBrowni <bacsik.barnabas@gmail.com>
2025-04-22 09:34:34 +0200

    Update ajanlatkeres.php

--- aloldalak/ajanlatkeres.php
+++ aloldalak/ajanlatkeres.php
@@ -67,7 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <title>Ajánlatkérés – BNBK Agro</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <?php include 'partials/navbar.php'; ?>
+  <?php include '../partials/navbar.php'; ?>
 
 </head>
 <body class="bg-light">
